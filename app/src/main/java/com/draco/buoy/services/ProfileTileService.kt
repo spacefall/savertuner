9cb178f20d9cf2988d600cb5e0073328737df753
MDP43140 <MDP43140@noreply.codeberg.org>
2024-12-20 18:01:21 +0700

    REUSE compliant
    See https://spdx.dev/learn/handling-license-info for details

--- app/src/main/java/com/draco/buoy/services/ProfileTileService.kt
+++ app/src/main/java/com/draco/buoy/services/ProfileTileService.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 tytydraco
+ * SPDX-FileCopyrightText: 2023-2024 s1m
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 package com.draco.buoy.services
 
 import android.content.SharedPreferences
